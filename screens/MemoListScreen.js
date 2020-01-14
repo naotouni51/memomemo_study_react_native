6afe9c0d4608635be96ae5338bd5591c366b20c1
naotouni51 <naotouni51@gmail.com>
2020-01-14 16:40:45 +0900

    クラス型コンポーネントにし、メモ一覧画面を追加

--- screens/MemoListScreen.js
+++ screens/MemoListScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class LoginScreen extends React.Component {
+export default class MemoListScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>ログイン画面</Text>
+        <Text>メモリスト画面</Text>
       </View>
     );
   }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>サインアップ画面</Text>
-    </View>
-  );
+export default class SignupScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>サインアップ画面</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
