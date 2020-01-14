19fee9dd3089bbf090119643478f3bae63b33e68
naotouni51 <naotouni51@gmail.com>
2020-01-14 17:59:42 +0900

    メモ作成画面のuiを整え、作成ボタンを押すと画面が戻るように

--- screens/MemoCreateScreen.js
+++ screens/MemoCreateScreen.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default class MemoCreateScreen extends React.Component {
+  state = {
+    content: ''
+  }
+
+  createMemo() {
+    this.props.navigation.goBack()
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>メモ作成画面</Text>
+        <View style={styles.textInputContainer}>
+          <Text>メモ作成画面</Text>
+          <TextInput
+            style={styles.memoEditInput}
+            multiline
+            value={this.state.content}
+            textAlignVertical="top"
+            onChangeText={ (text) => {this.setState({ content: text })} }
+          />
+        </View>
+        <View style={styles.saveButton}>
+          <TouchableOpacity onPress={() => this.createMemo()}>
+            <Text style={styles.saveButtonText}>保存！</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -15,7 +37,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  textInputContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  memoEditInput: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  saveButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'skyblue',
+    height: 50
+  },
+  saveButtonText: {
+    fontSize: 30
+  }
 });
